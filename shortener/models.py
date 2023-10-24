ca3123b7559dfb4464db90ff0359d862d52e24cc
rowanpark <rowan.park.83@gmail.com>
2023-10-24 10:58:00 +0900

    🗃️ 'shortener/models.py' 파일 수정
    - UserDetail 모델 비활성화

--- shortener/models.py
+++ shortener/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+# from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+
 # Create your models here.
 
 
@@ -8,4 +11,13 @@ class PayPlan(models.Model):
     price = models.IntegerField()
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
+
+class Users(AbstractUser):
+    pay_plan = models.ForeignKey(PayPlan, on_delete=models.DO_NOTHING, null=True)
+
+
+# class UserDetail(models.Model):
+#     # user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     user = models.OneToOneField(Users, on_delete=models.CASCADE)
+#     pay_plan = models.ForeignKey(PayPlan, on_delete=models.DO_NOTHING)
