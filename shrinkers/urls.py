5782be4948c4a7aca612edcc72960bd1509ba3ab
rowanpark <rowan.park.83@gmail.com>
2023-11-16 15:26:56 +0900

    ➕ urlpatterns 목록에 register, login, logout 추가

--- shrinkers/urls.py
+++ shrinkers/urls.py
@@ -14,18 +14,28 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 import debug_toolbar
+
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path  # include: django 2.0 이후 버전에서 사용되는 것
+# from django.conf.urls import include  # django 2.0 이전 버전과의 하위 호환성을 제공
+# from django.urls import path
 
 # from shortener.views import index, redirect_test
-from shortener.views import index, get_user
+from shortener.views import index, get_user, register, login_view, logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('__debug__', include(debug_toolbar.urls)),  # Django Debug Toolbar
-    path('', index, name='index_name'),
+    path('__debug__/', include(debug_toolbar.urls)),  # Django Debug Toolbar
+
+    # path('', index, name='index_name'),
+    path('', index, name='index'),
     # path('redirect', redirect_test),
     path('get_user/<int:user_id>', get_user),
+    
+    path('register', register, name='register'),
+    path('login', login_view, name='login'),
+    path('logout', logout_view, name='logout'),
 ]
