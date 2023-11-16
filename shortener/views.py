7daad954cf60d9f18845acd943bdfe1b5e9f8e0e
rowanpark <rowan.park.83@gmail.com>
2023-11-16 15:17:21 +0900

    ✨ register, login_view, logout_view 뷰 추가
    - 'shortener/views.py' 파일에 코드 추가
    - 불필요한 부분 주석 처리

--- shortener/views.py
+++ shortener/views.py
@@ -1,22 +1,29 @@
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import AuthenticationForm
 from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from django.views.decorators.csrf import csrf_exempt
+
+from shortener.forms import RegisterForm
 from shortener.models import Users
 
 # Create your views here.
 
 def index(request):
+    # print(request.user.pay_plan)
+    # print(request.user.pay_plan.name)
+    
     user = Users.objects.filter(username='rowanpark').first()
     # user = Users.objects.get(username='rowanpark')
     email = user.email if user else 'Anonymous User!'
+
+    print('Logged in?', request.user.is_authenticated)  # 로그인 유무
+    if request.user.is_authenticated is False:
+        email = 'Anonymous User!'
+
     print(email)
 
-    # print(request.user.is_authenticated)  # 로그인 유무
-    # if request.user.is_authenticated is False:
-    #     email = 'Anonymous User!'
-    #     print(email)
-
-    return render(request, 'base.html', {'welcome_msg': f'Hello {email}', 'hello': 'hello!'})
+    return render(request, 'base.html', {'welcome_msg': f'Welcome! {email}', 'hello': 'Hello!'})
 
 # def redirect_test(request):
 #     print('Go Redirect')
@@ -37,3 +44,41 @@ def get_user(request, user_id):
             user = Users.objects.filter(pk=user_id).update(username=username)
 
         return JsonResponse(status=201, data=dict(msg='You just reached with Post Method!'), safe=False)  # 한글을 보낼 경우에 'safe=False'
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        msg = '올바르지 않은 데이터 입니다.'
+        if form.is_valid():
+            form.save() 
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            msg = '회원가입 완료'
+        return render(request, 'register.html', {'form': form, 'msg': msg})
+    else:
+        form = RegisterForm()
+        return render(request, 'register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        msg = '가입되어 있지 않거나 로그인 정보가 잘못 되었습니다.'
+        print(form.is_valid)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=raw_password)
+            if user is not None:
+                msg = '로그인 성공'
+                login(request, user)
+        return render(request, 'base.html', {'msg': msg})
+        # return render(request, 'login.html', {'form': form, 'msg': msg})
+    else:
+        form = AuthenticationForm()
+        return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
