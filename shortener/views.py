a29f9c638b546cb78ba364b1f19c64dc9e9dd2dc
rowanpark <rowan.park.83@gmail.com>
2023-12-06 17:43:22 +0900

    💄 로그인 화면에 'Impact Design System' 적용
    - Impact Design System - v1.0.0
    
    - Product Page: https://www.creative-tim.com/product/impact-design-system
    - Copyright 2010 Creative Tim (https://www.creative-tim.com)
    - Licensed under MIT (https://github.com/creativetimofficial/impact-design-system/blob/master/LICENSE.md)

--- shortener/views.py
+++ shortener/views.py
@@ -64,22 +64,33 @@ def register(request):
         return render(request, 'register.html', {'form': form})
 
 def login_view(request):
+    msg = None
+    is_ok = False
     if request.method == 'POST':
         form = AuthenticationForm(request, request.POST)
-        msg = '가입되어 있지 않거나 로그인 정보가 잘못 되었습니다.'
-        print(form.is_valid)
+        # msg = '가입되어 있지 않거나 로그인 정보가 잘못 되었습니다.'
+        # print(form.is_valid)
         if form.is_valid():
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=raw_password)
             if user is not None:
-                msg = '로그인 성공'
+                # msg = '로그인 성공'
                 login(request, user)
-        return render(request, 'base.html', {'msg': msg})
+                is_ok = True
+        else:
+            msg = '올바른 유저ID와 패스워드를 입력하세요.'
+        # return render(request, 'base.html', {'msg': msg})
         # return render(request, 'login.html', {'form': form, 'msg': msg})
     else:
         form = AuthenticationForm()
-        return render(request, 'login.html', {'form': form})
+        # return render(request, 'login.html', {'form': form})
+
+    for visible in form.visible_fields():
+        visible.field.widget.attrs['placehoder'] = '유저ID' if visible.name == 'username' else '패스워드'
+        visible.field.widget.attrs['class'] = 'form-control'
+
+    return render(request, 'login.html', {'form': form, 'msg': msg, 'is_ok': is_ok})
 
 def logout_view(request):
     logout(request)
