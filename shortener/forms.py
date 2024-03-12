9eb009b62c114c4bad23355d914044b01f5d1177
rowanpark <rowan.park.83@gmail.com>
2024-03-12 11:07:53 +0900

    ✨ '단축 URL' CRUD 기능 추가
    - '게시판 만들기'와 유사

--- shortener/forms.py
+++ shortener/forms.py
@@ -1,6 +1,8 @@
+from typing import Any
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from shortener.models import Users
+from django.utils.translation import gettext_lazy as _
+from shortener.models import ShortenedUrls, Users
 
 
 class RegisterForm(UserCreationForm):
@@ -18,6 +20,7 @@ class RegisterForm(UserCreationForm):
             'password2',
         )
 
+
 class LoginForm(forms.Form):
     email = forms.CharField(
         max_length=100,
@@ -34,3 +37,34 @@ class LoginForm(forms.Form):
         disabled=False,
         widget=forms.CheckboxInput(attrs={'class': 'custom-control-input', 'id': '_loginRememberMe'})
     )
+
+
+class UrlCreateForm(forms.ModelForm):
+    class Meta:
+        model = ShortenedUrls
+        fields = ['nick_name', 'target_url']
+        lables = {
+            'nick_name': _('별칭'),
+            'target_url': _('URL'),
+        }
+        widgets = {
+            'nick_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'URL을 구분하기 위한 별칭'}),
+            'target_url': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '포워딩될 URL'}),
+        }
+
+    # 재정의
+    def save(self, request, commit=True):
+        instance = super(UrlCreateForm, self).save(commit=False)
+        instance.created_by_id = request.user.id
+        instance.target_url = instance.target_url.strip()
+        if commit:
+            instance.save()
+        return instance
+
+    # 재정의
+    def update_form(self, request, url_id):
+        instance = super(UrlCreateForm, self).save(commit=False)
+        instance.target_url = instance.target_url.strip()
+        ShortenedUrls.objects.filter(pk=url_id, created_by_id=request.user.id).update(
+            target_url = instance.target_url, nick_name=instance.nick_name
+        )
--- shrinkers/urls.py
+++ shrinkers/urls.py
@@ -21,9 +21,22 @@ from django.contrib import admin
 from django.urls import include, path  # include: django 2.0 이후 버전에서 사용되는 것
 # from django.conf.urls import include  # django 2.0 이전 버전과의 하위 호환성을 제공
 # from django.urls import path
-
 # from shortener.views import index, redirect_test
-from shortener.views import index, get_user, register, login_view, logout_view, list_view, url_list
+from shortener.views import (
+    index,
+    get_user,
+    register,
+
+    login_view,
+    logout_view,
+
+    list_view,
+
+    url_list,
+    url_create,
+    url_change,
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -38,7 +51,11 @@ urlpatterns = [
     path('register', register, name='register'),
     path('login', login_view, name='login'),
     path('logout', logout_view, name='logout'),
-    
+
     path('list', list_view, name='list_view'),
-    path('list/url', url_list, name='url_list'),
+
+    # path('list/url', url_list, name='url_list'),
+    path('urls', url_list, name='url_list'),
+    path('urls/create', url_create, name='url_create'),
+    path('urls/<str:action>/<int:url_id>', url_change, name='url_change'),
 ]
