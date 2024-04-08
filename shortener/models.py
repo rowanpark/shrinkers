637b1af72f54a0430ad499d0a072a068841c7c1f
rowanpark <rowan.park.83@gmail.com>
2024-04-08 11:10:06 +0900

    🗃️ Statistic 모델 추가

--- shortener/models.py
+++ shortener/models.py
@@ -3,6 +3,7 @@ import string
 
 from django.db import models
 from django.contrib.auth.models import User as U
+from django.contrib.gis.geoip2 import GeoIP2
 
 
 class TimeStampedModel(models.Model):
@@ -68,6 +69,7 @@ class ShortenedUrls(TimeStampedModel):
     prefix = models.CharField(max_length=50, default=rand_letter)
     target_url = models.CharField(max_length=2000)
     shortened_url = models.CharField(max_length=6, default=rand_string)
+    click = models.BigIntegerField(default=0)
     creator = models.ForeignKey(Users, on_delete=models.CASCADE)
     created_via = models.CharField(max_length=8, choices=UrlCreatedVia.choices, default=UrlCreatedVia.WEBSITE)
     expired_at = models.DateTimeField(null=True)
@@ -81,3 +83,55 @@ class ShortenedUrls(TimeStampedModel):
                 ]
             ),
         ]
+
+    def clicked(self):
+        self.click += 1
+        self.save()
+
+
+class Statistic(TimeStampedModel):
+    class ApproachDevice(models.TextChoices):
+        PC = 'pc'
+        MOBILE = 'mobile'
+        TABLET = 'tablet'
+
+    shortened_url = models.ForeignKey(ShortenedUrls, on_delete=models.CASCADE)
+    ip = models.CharField(max_length=15)
+    web_browser = models.CharField(max_length=50)
+    device = models.CharField(max_length=6, choices=ApproachDevice.choices)
+    device_os = models.CharField(max_length=30)
+    country_code = models.CharField(max_length=2, default='XX')
+    country_name = models.CharField(max_length=100, default='UNKNOWN')
+
+    def record(self, request, url: ShortenedUrls):
+        self.shortened_url = url
+        self.ip = request.META['REMOTE_ADDR']
+        self.web_browser = request.user_agent.browser.family
+        ### 0
+        # if request.user_agent.is_mobile:
+        #     self.device = self.ApproachDevice.MOBILE
+        # elif request.user_agent.is_tablet:
+        #     self.device = self.ApproachDevice.TABLET
+        # else:
+        #     self.device = self.ApproachDevice.PC
+        ### 1
+        self.device = self.ApproachDevice.MOBILE if request.user_agent.is_mobile else self.ApproachDevice.TABLET if request.user_agent.is_tablet else self.ApproachDevice.PC
+        ### 2
+        # self.device = self.ApproachDevice.MOBILE if request.user_agent.is_mobile else \
+        #               self.ApproachDevice.TABLET if request.user_agent.is_tablet else \
+        #               self.ApproachDevice.PC
+        ### 3
+        # self.device = (
+        #     self.ApproachDevice.MOBILE if request.user_agent.is_mobile else 
+        #     self.ApproachDevice.TABLET if request.user_agent.is_tablet else 
+        #     self.ApproachDevice.PC
+        # )
+        self.device_os = request.user_agent.os.family
+        try:
+            country = GeoIP2().country(self.ip)
+            self.country_code = country.get('country_code', 'XX')
+            self.country_name = country.get('country_name', 'UNKNOWN')
+        except:
+            pass
+        url.clicked()
+        self.save()
