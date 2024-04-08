14cf8c36b3a42786bb8f178ad6971ad20fef96cc
rowanpark <rowan.park.83@gmail.com>
2024-04-08 13:44:00 +0900

    💡 주석 추가

--- shortener/utils.py
+++ shortener/utils.py
@@ -4,6 +4,8 @@ from django.db.models import F
 
 def url_count_changer(request, is_increase: bool):
     count_number = 1 if is_increase else -1
+    ### 1
     Users.objects.filter(user_id=request.user.id).update(url_count=F('url_count') + count_number)
+    ### 2
     # user = Users.objects.get(id=request.user.id)
     # Users.objects.filter(user=user).update(url_count=F('url_count') + count_number)
