3ca57a2522fa71a1a8d3de9f99d2ff5c5cef9644
rowanpark <rowan.park.83@gmail.com>
2024-04-15 16:37:15 +0900

    ✏️ 부적절한 필드명 수정
    - created_by_id -> creator_id

--- shortener/forms.py
+++ shortener/forms.py
@@ -62,7 +62,7 @@ class UrlCreateForm(forms.ModelForm):
             try:
                 instance.save()
             except Exception as e:
-                print(e)
+                print('UrlCreateForm save Error:', e)
             else:
                 url_count_changer(request, True)  # save를 성공 할때만 count
         return instance
@@ -71,6 +71,6 @@ class UrlCreateForm(forms.ModelForm):
     def update_form(self, request, url_id):
         instance = super(UrlCreateForm, self).save(commit=False)
         instance.target_url = instance.target_url.strip()
-        ShortenedUrls.objects.filter(pk=url_id, created_by_id=request.user.id).update(
+        ShortenedUrls.objects.filter(pk=url_id, creator_id=request.user.id).update(
             target_url = instance.target_url, nick_name=instance.nick_name
         )
