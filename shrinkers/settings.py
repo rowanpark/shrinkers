6b041de60a997b2e727f989ade86a85c464d4f42
rowanpark <rowan.park.83@gmail.com>
2024-04-15 16:26:36 +0900

    🔧 누락된 콤마(,) 추가

--- shrinkers/settings.py
+++ shrinkers/settings.py
@@ -65,7 +65,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 20
+    'PAGE_SIZE': 20,
 }
 
 INTERNAL_IPS = [
